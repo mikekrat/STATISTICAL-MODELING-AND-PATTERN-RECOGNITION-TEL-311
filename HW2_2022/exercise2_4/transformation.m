ea5d624fb3402de33fb5d5e261d38260a474a9c9
mikekrat <93589352+mikekrat@users.noreply.github.com>
2025-09-01 19:57:53 +0300

    Delete HW2_2022 directory

--- HW2_2022/exercise2_4/transformation.m
+++
@@ -1,9 +0,0 @@
-function out = transformation(X)
-% MAPFEATURE Feature mapping function to polynomial features
-
-out = zeros(size(X));
-
-out(:,1) = X(:,1) - (X(:,1) .^ 2 + X(:,2) .^ 2) - 4;
-out(:,2) = X(:,2) - (X(:,1) .^ 2 + X(:,2) .^ 2) - 4;
-
-end
